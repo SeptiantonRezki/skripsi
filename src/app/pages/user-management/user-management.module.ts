977123226eacf417f63b376ce630749abb36bbab
Asen-PC\Asen <asen@lighthouse.co.id>
2018-08-29 18:43:05 +0700

    page list admin principal

--- src/app/pages/user-management/user-management.module.ts
+++ src/app/pages/user-management/user-management.module.ts
@@ -4,12 +4,22 @@ import { CommonModule } from '@angular/common';
 import { UserManagementRoutingModule } from './user-management-routing.module';
 import { AdminPrincipalIndexComponent } from './admin-principal/index/admin-principal-index.component';
 import { FieldForceIndexComponent } from './field-force/index/field-force-index.component';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { MatButtonModule, MatCheckboxModule, MatIconModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
-    UserManagementRoutingModule
+    UserManagementRoutingModule,
+    NgxDatatableModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule
   ],
-  declarations: [AdminPrincipalIndexComponent, FieldForceIndexComponent]
+  declarations: [AdminPrincipalIndexComponent, FieldForceIndexComponent],
+  
+   exports     : [
+        AdminPrincipalIndexComponent, FieldForceIndexComponent
+    ]
 })
 export class UserManagementModule { }
