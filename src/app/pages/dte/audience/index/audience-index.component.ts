47501ba4148cf1650102d1a285e76f31172061b7
Nouval <nouval@lighthouse.co.id>
2018-09-09 01:40:58 +0700

    list scheduler trade program & audience

--- src/app/pages/dte/audience/index/audience-index.component.ts
+++ src/app/pages/dte/audience/index/audience-index.component.ts
@@ -1,15 +1,144 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, TemplateRef } from '@angular/core';
+import { Page } from 'app/classes/laravel-pagination';
+import { DatatableComponent } from '../../../../../../node_modules/@swimlane/ngx-datatable';
+import { Subject, Observable } from '../../../../../../node_modules/rxjs';
+import { Router } from '../../../../../../node_modules/@angular/router';
+import { DialogService } from 'app/services/dialog.service';
+import { AudienceService } from '../../../../services/dte/audience.service';
 
 @Component({
   selector: 'app-audience-index',
   templateUrl: './audience-index.component.html',
   styleUrls: ['./audience-index.component.scss']
 })
-export class AudienceIndexComponent implements OnInit {
+export class AudienceIndexComponent {
 
-  constructor() { }
+  rows: any[];
+  selected: any[];
+  id: any;
+
+  loadingIndicator = true;
+  reorderable = true;
+  pagination: Page = new Page();
+  onLoad: boolean;
+
+  @ViewChild(DatatableComponent)
+  table: DatatableComponent;
+
+  @ViewChild("activeCell")
+  activeCellTemp: TemplateRef<any>;
+
+  keyUp = new Subject<string>();
+  
+  constructor(
+    private router: Router,
+    private dialogService: DialogService,
+    private audienceService: AudienceService,
+  ) {
+    this.onLoad = true;
+    this.selected = [];
+
+    const observable = this.keyUp.debounceTime(1000)
+      .distinctUntilChanged()
+      .flatMap(search => {
+        return Observable.of(search).delay(500);
+      })
+      .subscribe(data => {
+        this.updateFilter(data);
+      });
+  }
 
   ngOnInit() {
+    this.getAudience();
+  }
+
+  getAudience() {
+    this.audienceService.get(this.pagination).subscribe(
+      res => {
+        Page.renderPagination(this.pagination, res);
+        this.rows = res.data;
+        this.onLoad = false;
+        this.loadingIndicator = false;
+      },
+      err => {
+        this.onLoad = false;
+      }
+    );
+  }
+
+  onSelect({ selected }) {
+    this.selected.splice(0, this.selected.length);
+    this.selected.push(...selected);
+
+    console.log("Select Event", selected, this.selected);
+  }
+
+  setPage(pageInfo) {
+    this.loadingIndicator = true;
+    this.pagination.page = pageInfo.offset + 1;
+
+    this.audienceService.get(this.pagination).subscribe(res => {
+      Page.renderPagination(this.pagination, res);
+      this.rows = res.data;
+
+      this.loadingIndicator = false;
+    });
+  }
+
+  onSort(event) {
+    this.pagination.sort = event.column.prop;
+    this.pagination.sort_type = event.newValue;
+    this.pagination.page = 1;
+    this.loadingIndicator = true;
+
+    console.log(this.pagination)
+
+    this.audienceService.get(this.pagination).subscribe(res => {
+      Page.renderPagination(this.pagination, res);
+      this.rows = res.data;
+
+      this.loadingIndicator = false;
+    });
+  }
+
+  updateFilter(string) {
+    this.loadingIndicator = true;
+    this.table.offset = 0;
+    this.pagination.search = string;
+    this.pagination.page = 1;
+
+    this.audienceService.get(this.pagination).subscribe(res => {
+      Page.renderPagination(this.pagination, res);
+      this.rows = res.data;
+      
+      this.loadingIndicator = false;
+    });
+  }
+
+  getActives() {
+    return this.rows.map(row => row["active_status"]);
+  }
+
+  deleteAudience(id) {
+    this.id = id;
+    let data = {
+      titleDialog: "Hapus Audience",
+      captionDialog: "Apakah anda yakin untuk menghapus audience ini ?",
+      confirmCallback: this.confirmDelete.bind(this),
+      buttonText: ["Hapus", "Batal"]
+    };
+    this.dialogService.openCustomConfirmationDialog(data);
+  }
+
+  confirmDelete() {
+    this.audienceService.delete({ trade_program_id: this.id }).subscribe(res => {
+      if (res.status) {
+        this.dialogService.brodcastCloseConfirmation();
+        this.getAudience();
+
+        this.dialogService.openSnackBar({ message: "Data Berhasil Dihapus" });
+      }
+    });
   }
 
 }
