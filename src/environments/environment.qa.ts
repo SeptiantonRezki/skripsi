61115f682ac5e0dd977d31ca057af14b6c4f5cb9
Nouval Kurnia Firdaus <nouval@lighthouse.co.id>
2019-03-20 18:55:48 +0700

    update envi monolith qa

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   server: "https://api.qa.src.id",
-  label: "DEVELOPMENT",
-  show_label: true,
-  image: "assets/images/ayo/icon/dev.png",
-  image_2x: "assets/images/ayo/icon/dev@2x.png 2x"
+  label: "MONOLITH QA",
+  show_label: false,
+  image: "assets/images/ayo/AYO_SRC_(Master).png",
+  image_2x: "assets/images/ayo/AYO_SRC_(Master)@2x.png 2x"
 };
