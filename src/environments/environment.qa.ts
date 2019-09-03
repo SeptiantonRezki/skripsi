febffddb95fdd9cdc7b43cb212b63f2bd90e6a6e
Jois Andresky <jois@lighthouse.co.id>
2019-09-03 13:48:53 +0700

    env qa

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -1,10 +1,24 @@
 export const environment = {
   production: true,
-  server: "https://api.qa.micro.src.id",
-  server_service: "http://user.ayo-micro.dxtr.asia",
+  server: "https://api.qa.src.id",
+  server_service: "https://api.qa.src.id",
   label: "MONOLITH QA",
   show_label: false,
   image: "assets/images/ayo/AYO_SRC_(Master).png",
   image_2x: "assets/images/ayo/AYO_SRC_(Master)@2x.png 2x",
   backgroundImage: "assets/images/ayo/header/Header-Color-Red@2x.jpg"
 };
+
+export const serviceServer = (server) => {
+  return `https://${server}.api.qa.src.id`;
+}
+
+export const server = {
+  user: "users",
+  business: "businesses",
+  area: "areas",
+  auth: "auth",
+  newsfeed: "newsfeed",
+  banner: "banner",
+  content: "contentstatic"
+}
