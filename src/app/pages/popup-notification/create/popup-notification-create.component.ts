de24d46924d66442b751f2d99dcbb50c9e93753f
Jois Andresky <jois@lighthouse.co.id>
2019-09-26 12:29:59 +0700

    fix filter zone on PopUp notification Targeted Audience

--- src/app/pages/popup-notification/create/popup-notification-create.component.ts
+++ src/app/pages/popup-notification/create/popup-notification-create.component.ts
@@ -15,6 +15,9 @@ import { Subject } from 'rxjs';
 import { DatatableComponent, SelectionType } from '@swimlane/ngx-datatable';
 import { HttpErrorResponse } from '@angular/common/http';
 import { ImportPopUpAudienceComponent } from '../import-pop-up-audience/import-pop-up-audience.component';
+import { RetailerService } from 'app/services/user-management/retailer.service';
+import { CustomerService } from 'app/services/user-management/customer.service';
+import { WholesalerService } from 'app/services/user-management/wholesaler.service';
 
 @Component({
   selector: 'app-popup-notification-create',
@@ -84,7 +87,10 @@ export class PopupNotificationCreateComponent {
     private adapter: DateAdapter<any>,
     private formBuilder: FormBuilder,
     private _lightbox: Lightbox,
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private retailerService: RetailerService,
+    private customerService: CustomerService,
+    private wholesalerService: WholesalerService,
   ) {
     this.adapter.setLocale('id');
     this.areaFromLogin = this.dataService.getDecryptedProfile()['area_type'];
@@ -226,7 +232,9 @@ export class PopupNotificationCreateComponent {
           this.formPopupGroup.controls['date_ws_downline'].enable();
         }
       }
-      if (this.formPopupGroup.get("is_target_audience").value === true) this.getAudience();
+      if (this.formPopupGroup.get("is_target_audience").value === true) {
+        this.getAudience();
+      };
 
       this.formPopupGroup.controls['landing_page'].setValue('');
     });
@@ -268,10 +276,163 @@ export class PopupNotificationCreateComponent {
         this.formPopupGroup.controls['date_ws_downline'].disable();
       }
       if (this.formPopupGroup.get("is_target_audience").value === true) this.getAudience();
-    })
+    });
+
+    this.formFilter.valueChanges.subscribe(filter => {
+      if (this.formPopupGroup.get("is_target_audience").value === true) this.getAudience();
+    });
 
     this.addArea();
+    this.initFilterArea();
     // if(this.formPopupGroup.get("is_target_audience").value === true) this.getAudience();
+  }
+
+  initFilterArea() {
+    this.areaFromLogin.map(item => {
+      let level_desc = '';
+      switch (item.type.trim()) {
+        case 'national':
+          level_desc = 'zone';
+          this.formFilter.get('national').setValue(item.id);
+          this.formFilter.get('national').disable();
+          break
+        case 'division':
+          level_desc = 'region';
+          this.formFilter.get('zone').setValue(item.id);
+          this.formFilter.get('zone').disable();
+          break;
+        case 'region':
+          level_desc = 'area';
+          this.formFilter.get('region').setValue(item.id);
+          this.formFilter.get('region').disable();
+          break;
+        case 'area':
+          level_desc = 'salespoint';
+          this.formFilter.get('area').setValue(item.id);
+          this.formFilter.get('area').disable();
+          break;
+        case 'salespoint':
+          level_desc = 'district';
+          this.formFilter.get('salespoint').setValue(item.id);
+          this.formFilter.get('salespoint').disable();
+          break;
+        case 'district':
+          level_desc = 'territory';
+          this.formFilter.get('district').setValue(item.id);
+          this.formFilter.get('district').disable();
+          break;
+        case 'territory':
+          this.formFilter.get('territory').setValue(item.id);
+          this.formFilter.get('territory').disable();
+          break;
+      }
+      this.getAudienceArea(level_desc, item.id);
+    });
+  }
+
+  getAudienceArea(selection, id) {
+    let item: any;
+    switch (selection) {
+      case 'zone':
+        this.retailerService.getListOtherChildren({ parent_id: id }).subscribe(res => {
+          this.list[selection] = res;
+        });
+
+        this.formFilter.get('region').setValue('');
+        this.formFilter.get('area').setValue('');
+        this.formFilter.get('salespoint').setValue('');
+        this.formFilter.get('district').setValue('');
+        this.formFilter.get('territory').setValue('');
+        this.list['region'] = [];
+        this.list['area'] = [];
+        this.list['salespoint'] = [];
+        this.list['district'] = [];
+        this.list['territory'] = [];
+        break;
+      case 'region':
+        item = this.list['zone'].length > 0 ? this.list['zone'].filter(item => item.id === id)[0] : {};
+        if (item.name !== 'all') {
+          this.retailerService.getListOtherChildren({ parent_id: id }).subscribe(res => {
+            this.list[selection] = res;
+          });
+        } else {
+          this.list[selection] = []
+        }
+
+        this.formFilter.get('region').setValue('');
+        this.formFilter.get('area').setValue('');
+        this.formFilter.get('salespoint').setValue('');
+        this.formFilter.get('district').setValue('');
+        this.formFilter.get('territory').setValue('');
+        this.list['area'] = [];
+        this.list['salespoint'] = [];
+        this.list['district'] = [];
+        this.list['territory'] = [];
+        break;
+      case 'area':
+        item = this.list['region'].length > 0 ? this.list['region'].filter(item => item.id === id)[0] : {};
+        if (item.name !== 'all') {
+          this.retailerService.getListOtherChildren({ parent_id: id }).subscribe(res => {
+            this.list[selection] = res;
+          });
+        } else {
+          this.list[selection] = []
+        }
+
+        this.formFilter.get('area').setValue('');
+        this.formFilter.get('salespoint').setValue('');
+        this.formFilter.get('district').setValue('');
+        this.formFilter.get('territory').setValue('');
+        this.list['salespoint'] = [];
+        this.list['district'] = [];
+        this.list['territory'] = [];
+        break;
+      case 'salespoint':
+        item = this.list['area'].length > 0 ? this.list['area'].filter(item => item.id === id)[0] : {};
+        if (item.name !== 'all') {
+          this.retailerService.getListOtherChildren({ parent_id: id }).subscribe(res => {
+            this.list[selection] = res;
+          });
+        } else {
+          this.list[selection] = []
+        }
+
+        this.formFilter.get('salespoint').setValue('');
+        this.formFilter.get('district').setValue('');
+        this.formFilter.get('territory').setValue('');
+        this.list['district'] = [];
+        this.list['territory'] = [];
+        break;
+      case 'district':
+        item = this.list['salespoint'].length > 0 ? this.list['salespoint'].filter(item => item.id === id)[0] : {};
+        if (item.name !== 'all') {
+          this.retailerService.getListOtherChildren({ parent_id: id }).subscribe(res => {
+            this.list[selection] = res;
+          });
+        } else {
+          this.list[selection] = []
+        }
+
+        this.formFilter.get('district').setValue('');
+        this.formFilter.get('territory').setValue('');
+        this.list['territory'] = [];
+        break;
+      case 'territory':
+        item = this.list['district'].length > 0 ? this.list['district'].filter(item => item.id === id)[0] : {};
+        if (item.name !== 'all') {
+          this.retailerService.getListOtherChildren({ parent_id: id }).subscribe(res => {
+            this.list[selection] = res;
+          });
+        } else {
+          this.list[selection] = []
+        }
+
+        this.formFilter.get('territory').setValue('');
+        break;
+
+      default:
+        break;
+    }
   }
 
   createArea(): FormGroup {
@@ -754,6 +915,8 @@ export class PopupNotificationCreateComponent {
   }
 
   getAudience() {
+    let areaSelected = Object.entries(this.formFilter.getRawValue()).map(([key, value]) => ({ key, value })).filter(item => item.value !== "");
+    this.pagination.area = areaSelected[areaSelected.length - 1].value;
     this.pagination['audience'] = this.formPopupGroup.get("user_group").value;
     if (this.formPopupGroup.get("user_group").value === 'retailer') {
       this.pagination['retailer_type'] = this.formPopupGroup.get("group_type").value;
@@ -853,6 +1016,10 @@ export class PopupNotificationCreateComponent {
   }
 
   async export() {
+    if (this.audienceSelected.length === 0) {
+      this.dialogService.openSnackBar({ message: 'Pilih audience untuk di ekspor!' });
+      return;
+    }
     this.dataService.showLoading(true);
     let body = this.audienceSelected.map(aud => aud.id);
     this.exportAccessCashier = true;
--- src/app/pages/popup-notification/edit/popup-notification-edit.component.ts
+++ src/app/pages/popup-notification/edit/popup-notification-edit.component.ts
@@ -15,6 +15,7 @@ import { Page } from 'app/classes/laravel-pagination';
 import { Subject } from 'rxjs';
 import { HttpErrorResponse } from '@angular/common/http';
 import { ImportPopUpAudienceComponent } from '../import-pop-up-audience/import-pop-up-audience.component';
+import { RetailerService } from 'app/services/user-management/retailer.service';
 
 @Component({
   selector: 'app-popup-notification-edit',
@@ -89,7 +90,8 @@ export class PopupNotificationEditComponent {
     private adapter: DateAdapter<any>,
     private formBuilder: FormBuilder,
     private _lightbox: Lightbox,
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private retailerService: RetailerService
   ) {
     this.adapter.setLocale('id');
     this.areaFromLogin = this.dataService.getDecryptedProfile()['area_type'];
@@ -287,9 +289,163 @@ export class PopupNotificationEditComponent {
       }
 
       if (this.formPopupGroup.controls["is_target_audience"].value === true) this.getAudience();
-    })
+    });
+
+    this.formFilter.valueChanges.subscribe(filter => {
+      if (this.formPopupGroup.get("is_target_audience").value === true) this.getAudience();
+    });
+
+    this.initFilterArea();
 
     this.getDetails();
+  }
+
+  initFilterArea() {
+    this.areaFromLogin.map(item => {
+      let level_desc = '';
+      switch (item.type.trim()) {
+        case 'national':
+          level_desc = 'zone';
+          this.formFilter.get('national').setValue(item.id);
+          this.formFilter.get('national').disable();
+          break
+        case 'division':
+          level_desc = 'region';
+          this.formFilter.get('zone').setValue(item.id);
+          this.formFilter.get('zone').disable();
+          break;
+        case 'region':
+          level_desc = 'area';
+          this.formFilter.get('region').setValue(item.id);
+          this.formFilter.get('region').disable();
+          break;
+        case 'area':
+          level_desc = 'salespoint';
+          this.formFilter.get('area').setValue(item.id);
+          this.formFilter.get('area').disable();
+          break;
+        case 'salespoint':
+          level_desc = 'district';
+          this.formFilter.get('salespoint').setValue(item.id);
+          this.formFilter.get('salespoint').disable();
+          break;
+        case 'district':
+          level_desc = 'territory';
+          this.formFilter.get('district').setValue(item.id);
+          this.formFilter.get('district').disable();
+          break;
+        case 'territory':
+          this.formFilter.get('territory').setValue(item.id);
+          this.formFilter.get('territory').disable();
+          break;
+      }
+      this.getAudienceArea(level_desc, item.id);
+    });
+  }
+
+  getAudienceArea(selection, id) {
+    let item: any;
+    switch (selection) {
+      case 'zone':
+        this.retailerService.getListOtherChildren({ parent_id: id }).subscribe(res => {
+          this.list[selection] = res;
+        });
+
+        this.formFilter.get('region').setValue('');
+        this.formFilter.get('area').setValue('');
+        this.formFilter.get('salespoint').setValue('');
+        this.formFilter.get('district').setValue('');
+        this.formFilter.get('territory').setValue('');
+        this.list['region'] = [];
+        this.list['area'] = [];
+        this.list['salespoint'] = [];
+        this.list['district'] = [];
+        this.list['territory'] = [];
+        break;
+      case 'region':
+        item = this.list['zone'].length > 0 ? this.list['zone'].filter(item => item.id === id)[0] : {};
+        if (item.name !== 'all') {
+          this.retailerService.getListOtherChildren({ parent_id: id }).subscribe(res => {
+            this.list[selection] = res;
+          });
+        } else {
+          this.list[selection] = []
+        }
+
+        this.formFilter.get('region').setValue('');
+        this.formFilter.get('area').setValue('');
+        this.formFilter.get('salespoint').setValue('');
+        this.formFilter.get('district').setValue('');
+        this.formFilter.get('territory').setValue('');
+        this.list['area'] = [];
+        this.list['salespoint'] = [];
+        this.list['district'] = [];
+        this.list['territory'] = [];
+        break;
+      case 'area':
+        item = this.list['region'].length > 0 ? this.list['region'].filter(item => item.id === id)[0] : {};
+        if (item.name !== 'all') {
+          this.retailerService.getListOtherChildren({ parent_id: id }).subscribe(res => {
+            this.list[selection] = res;
+          });
+        } else {
+          this.list[selection] = []
+        }
+
+        this.formFilter.get('area').setValue('');
+        this.formFilter.get('salespoint').setValue('');
+        this.formFilter.get('district').setValue('');
+        this.formFilter.get('territory').setValue('');
+        this.list['salespoint'] = [];
+        this.list['district'] = [];
+        this.list['territory'] = [];
+        break;
+      case 'salespoint':
+        item = this.list['area'].length > 0 ? this.list['area'].filter(item => item.id === id)[0] : {};
+        if (item.name !== 'all') {
+          this.retailerService.getListOtherChildren({ parent_id: id }).subscribe(res => {
+            this.list[selection] = res;
+          });
+        } else {
+          this.list[selection] = []
+        }
+
+        this.formFilter.get('salespoint').setValue('');
+        this.formFilter.get('district').setValue('');
+        this.formFilter.get('territory').setValue('');
+        this.list['district'] = [];
+        this.list['territory'] = [];
+        break;
+      case 'district':
+        item = this.list['salespoint'].length > 0 ? this.list['salespoint'].filter(item => item.id === id)[0] : {};
+        if (item.name !== 'all') {
+          this.retailerService.getListOtherChildren({ parent_id: id }).subscribe(res => {
+            this.list[selection] = res;
+          });
+        } else {
+          this.list[selection] = []
+        }
+
+        this.formFilter.get('district').setValue('');
+        this.formFilter.get('territory').setValue('');
+        this.list['territory'] = [];
+        break;
+      case 'territory':
+        item = this.list['district'].length > 0 ? this.list['district'].filter(item => item.id === id)[0] : {};
+        if (item.name !== 'all') {
+          this.retailerService.getListOtherChildren({ parent_id: id }).subscribe(res => {
+            this.list[selection] = res;
+          });
+        } else {
+          this.list[selection] = []
+        }
+
+        this.formFilter.get('territory').setValue('');
+        break;
+
+      default:
+        break;
+    }
   }
 
   async getDetails() {
@@ -886,6 +1042,8 @@ export class PopupNotificationEditComponent {
   }
 
   getAudience() {
+    let areaSelected = Object.entries(this.formFilter.getRawValue()).map(([key, value]) => ({ key, value })).filter(item => item.value !== "");
+    this.pagination.area = areaSelected[areaSelected.length - 1].value;
     this.pagination['audience'] = this.formPopupGroup.get("user_group").value;
     if (this.formPopupGroup.get("user_group").value === 'retailer') {
       this.pagination['retailer_type'] = this.formPopupGroup.get("group_type").value;
@@ -984,6 +1142,10 @@ export class PopupNotificationEditComponent {
   }
 
   async export() {
+    if (this.audienceSelected.length === 0) {
+      this.dialogService.openSnackBar({ message: 'Pilih audience untuk di ekspor!' });
+      return;
+    }
     this.dataService.showLoading(true);
     let body = this.audienceSelected.map(aud => aud.id);
     this.exportPopUpNotif = true;
