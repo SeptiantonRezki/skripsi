8f6a1a4b480f06c4baf1cbe538ec30748dd59024
Jois Andresky <jois@lighthouse.co.id>
2019-09-30 13:57:07 +0700

    first design

--- src/app/pages/dte/automation/create/audience-trade-program.component.ts
+++ src/app/pages/dte/automation/create/audience-trade-program.component.ts
@@ -1,20 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from 'app/services/data.service';
 import { MatTabChangeEvent } from '@angular/material';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-audience-trade-program',
   templateUrl: './audience-trade-program.component.html',
   styleUrls: ['./audience-trade-program.component.scss']
 })
-export class AudienceTradeProgramComponent {
+export class AudienceTradeProgramComponent implements OnInit {
   selectedTab: any;
+  formAutomation: FormGroup;
+  listAutomation: any[] = [
+    { name: 'E-Order', value: 'e-order' },
+    { name: 'Digital Coupon', value: 'coupon' },
+    { name: 'Referral Code', value: 'referral_code' }
+  ];
 
   constructor(
-    private dataService: DataService
+    private dataService: DataService,
+    private formBuilder: FormBuilder
   ) {
     // const selectedTab = dataService.getFromStorage("selected_tab");
     this.selectedTab = 0;
+  }
+
+  ngOnInit() {
+    this.formAutomation = this.formBuilder.group({
+      automation: ['e-order', Validators.required]
+    });
   }
 
   setSelectedTab(tabChangeEvent: MatTabChangeEvent) {
