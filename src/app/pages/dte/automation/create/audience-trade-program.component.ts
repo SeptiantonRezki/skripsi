fc52897e57a92a6990a22d411f83434a3969dcfd
Jois Andresky <jois@lighthouse.co.id>
2019-09-30 17:00:38 +0700

    2nd design

--- src/app/pages/dte/automation/create/audience-trade-program.component.ts
+++ src/app/pages/dte/automation/create/audience-trade-program.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from 'app/services/data.service';
 import { MatTabChangeEvent } from '@angular/material';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl, FormArray } from '@angular/forms';
+import { ReplaySubject, Subject } from 'rxjs';
+import { AudienceTradeProgramService } from 'app/services/dte-automation/audience-trade-program.service';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-audience-trade-program',
@@ -17,9 +20,16 @@ export class AudienceTradeProgramComponent implements OnInit {
     { name: 'Referral Code', value: 'referral_code' }
   ];
 
+  filteredTradeProgram: ReplaySubject<any[]> = new ReplaySubject<any[]>(1);
+  tradePrograms: any[];
+  public filterTradeProgram: FormControl = new FormControl();
+  private _onDestroy = new Subject<void>();
+  skus: FormArray;
+
   constructor(
     private dataService: DataService,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private audienceTradeProgramService: AudienceTradeProgramService
   ) {
     // const selectedTab = dataService.getFromStorage("selected_tab");
     this.selectedTab = 0;
@@ -27,8 +37,63 @@ export class AudienceTradeProgramComponent implements OnInit {
 
   ngOnInit() {
     this.formAutomation = this.formBuilder.group({
-      automation: ['e-order', Validators.required]
+      automation: ['e-order', Validators.required],
+      startDate: [new Date(), Validators.required],
+      endDate: [new Date(), Validators.required],
+      coin_max: [0, Validators.required],
+      coin_reward: [0, Validators.required],
+      coupon_total: [0],
+      trade_program_id: [""],
+      title_challenge: ["", Validators.required],
+      description_challenge: ["", Validators.required],
+      button_text: ["", Validators.required],
+      skus: this.formBuilder.array([this.createFormSku()])
     });
+
+    this.audienceTradeProgramService.getTradePrograms().subscribe(res => {
+      console.log('res list trade programs', res);
+      this.tradePrograms = res.data.slice();
+      this.filteredTradeProgram.next((res && res.data) ? res.data.slice() : []);
+    });
+
+    this.filterTradeProgram
+      .valueChanges
+      .pipe(
+        takeUntil(this._onDestroy)
+      )
+      .subscribe(() => {
+        this._filterTradeProgram();
+      });
+  }
+
+  createFormSku() {
+    return this.formBuilder.group({
+      id: [""],
+      name: [""]
+    });
+  }
+
+  addSkuProduct() {
+    this.skus = this.formAutomation.get("skus") as FormArray;
+    this.skus.insert(0, this.createFormSku());
+  }
+
+  _filterTradeProgram() {
+    if (!this.tradePrograms) {
+      return;
+    }
+    // get the search keyword
+    let search = this.filterTradeProgram.value;
+    if (!search) {
+      this.filteredTradeProgram.next(this.tradePrograms.slice());
+      return;
+    } else {
+      search = search.toLowerCase();
+    }
+    // filter the trades
+    this.filteredTradeProgram.next(
+      this.tradePrograms.filter(trade => trade.name.toLowerCase().indexOf(search) > -1)
+    );
   }
 
   setSelectedTab(tabChangeEvent: MatTabChangeEvent) {
