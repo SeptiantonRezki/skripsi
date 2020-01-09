da021f2882b954109f9899ed332c1b34b6ccab81
Yudi <yuddi.0708@gmail.com>
2020-01-10 00:48:05 +0700

    fix: Qiscus - Manajemen Template Pesan #3 - bug fix reset to default

--- src/app/pages/template-message-management/template-message/index/template-message.component.ts
+++ src/app/pages/template-message-management/template-message/index/template-message.component.ts
@@ -3,32 +3,69 @@ import { MatDialog } from '@angular/material';
 import { DialogService } from 'app/services/dialog.service';
 import { TemplateMessageService } from 'app/services/template-message-management/template-message.service';
 
-const defaultTemplate = [
-    {
-      "id": 0,
-      "title": "Hai, selamat datang di toko kami! Apakah ada informasi yang dapat kami berikan?",
-      "body": "Hai, selamat datang di toko kami! Apakah ada informasi yang dapat kami berikan?"
-    },
+const defaultTemplateWholesaler = [
     {
       "id": 1,
-      "title": "Pesanan Anda masih dalam proses untuk disiapkan.",
-      "body": "Pesanan Anda masih dalam proses untuk disiapkan."
+      "title": "Hai, selamat datang di toko kami! Apakah ada informasi yang dapat kami berikan?",
+      "body": "Hai, selamat datang di toko kami! Apakah ada informasi yang dapat kami berikan?",
+      "user": "wholesaler"
     },
     {
       "id": 2,
-      "title": "Mohon maaf ada perubahan pada pesanan Anda, apakah bisa dilakukan konfirmasi (tekan Setuju)?",
-      "body": "Mohon maaf ada perubahan pada pesanan Anda, apakah bisa dilakukan konfirmasi (tekan Setuju)?"
+      "title": "Pesanan Anda masih dalam proses untuk disiapkan.",
+      "body": "Pesanan Anda masih dalam proses untuk disiapkan.",
+      "user": "wholesaler"
     },
     {
       "id": 3,
-      "title": "Apakah pesanannya sudah diterima? Jika sudah, mohon untuk lakukan konfirmasi penerimaan (tekan Pesanan Diterima)",
-      "body": "Apakah pesanannya sudah diterima? Jika sudah, mohon untuk lakukan konfirmasi penerimaan (tekan Pesanan Diterima)"
+      "title": "Mohon maaf ada perubahan pada pesanan Anda, apakah bisa dilakukan konfirmasi (tekan Setuju)?",
+      "body": "Mohon maaf ada perubahan pada pesanan Anda, apakah bisa dilakukan konfirmasi (tekan Setuju)?",
+      "user": "wholesaler"
     },
     {
       "id": 4,
-      "title": "Terima kasih telah berbelanja di toko kami. Semoga pengalaman berbelanja Anda menyenangkan!\nSilahkan sampaikan kritik dan saran agar kami dapat melayani Anda lebih baik lagi.",
-      "body": "Terima kasih telah berbelanja di toko kami. Semoga pengalaman berbelanja Anda menyenangkan!\nSilahkan sampaikan kritik dan saran agar kami dapat melayani Anda lebih baik lagi."
+      "title": "Apakah pesanannya sudah diterima? Jika sudah, mohon untuk lakukan konfirmasi penerimaan (tekan Pesanan Diterima)",
+      "body": "Apakah pesanannya sudah diterima? Jika sudah, mohon untuk lakukan konfirmasi penerimaan (tekan Pesanan Diterima)",
+      "user": "wholesaler"
+    },
+    {
+      "id": 5,
+      "title": "Terima kasih telah berbelanja di toko kami. Semoga pengalaman berbelanja Anda menyenangkan!",
+      "body": "Terima kasih telah berbelanja di toko kami. Semoga pengalaman berbelanja Anda menyenangkan!\n\nSilahkan sampaikan kritik dan saran agar kami dapat melayani Anda lebih baik lagi.",
+      "user": "wholesaler"
     }
+];
+const defaultTemplateRetailer = [
+  {
+    "id": 6,
+    "title": "Hai, pesanan saya sudah diproses sampai mana?",
+    "body": "Hai, pesanan saya sudah diproses sampai mana?",
+    "user": "retailer"
+  },
+  {
+    "id": 7,
+    "title": "Mohon info, kapan pesanan saya akan dikirim?",
+    "body": "Mohon info, kapan pesanan saya akan dikirim?",
+    "user": "retailer"
+  },
+  {
+    "id": 8,
+    "title": "Mohon maaf ada perubahan pada pesanan Anda, apakah bisa dilakukan konfirmasi (tekan Setuju)?",
+    "body": "Mohon maaf ada perubahan pada pesanan Anda, apakah bisa dilakukan konfirmasi (tekan Setuju)?",
+    "user": "retailer"
+  },
+  {
+    "id": 9,
+    "title": "Apakah pesanan saya jumlah dan harganya sesuai dengan yang saya pesan diaplikasi?",
+    "body": "Apakah pesanan saya jumlah dan harganya sesuai dengan yang saya pesan diaplikasi?",
+    "user": "retailer"
+  },
+  {
+    "id": 10,
+    "title": "Terima kasih!",
+    "body": "Terima kasih!",
+    "user": "retailer"
+  }
 ];
 @Component({
   selector: 'app-template-message',
@@ -69,16 +106,10 @@ export class TemplateMessageComponent {
       this.retailerTemplates = res.data.retailer;
 
       if (res.data.wholesaler.length == 0) {
-        defaultTemplate.map((item: any) => {
-          item.user = 'wholesaler';
-          return this.wholesalerTemplates.push(item);
-        });
+        this.wholesalerTemplates = defaultTemplateWholesaler;
       }
       if (res.data.retailer.length == 0) {
-        defaultTemplate.map((item: any) => {
-          item.user = 'retailer';
-          return this.retailerTemplates.push(item);
-        });
+        this.retailerTemplates = defaultTemplateRetailer;
       } 
       this.onLoad = false;
     }, error => {
@@ -96,33 +127,30 @@ export class TemplateMessageComponent {
   }
 
   resetToDefault(index: number, user: string) {
+    console.log('index', index);
+    console.log('user', user);
     if (user == 'wholesaler') {
-      this.wholesalerTemplates[index].title = defaultTemplate[index].title;
-      this.wholesalerTemplates[index].body = defaultTemplate[index].body;
+      this.wholesalerTemplates[index].title = defaultTemplateWholesaler[index].title;
+      this.wholesalerTemplates[index].body = defaultTemplateWholesaler[index].body;
     } else {
       if (user == 'retailer') {
-        this.retailerTemplates[index].title = defaultTemplate[index].title;
-        this.retailerTemplates[index].body = defaultTemplate[index].body;
+        this.retailerTemplates[index].title = defaultTemplateRetailer[index].title;
+        this.retailerTemplates[index].body = defaultTemplateRetailer[index].body;
       }
     }
   }
 
+
   resetToDefaultAll(user: string) {
     if (user == 'wholesaler') {
-      this.deleteListWholesaler = this.wholesalerTemplates;
-      this.wholesalerTemplates = [];
-      defaultTemplate.map((item: any) => {
-        item.user = 'wholesaler';
-        return this.wholesalerTemplates.push(item);
-      });
+      const match = [ ...this.deleteListWholesaler, ...this.wholesalerTemplates]
+      this.deleteListWholesaler = match.reduce((a, c) => { !a.find(v => v.id === c.id) && a.push(c); return a; }, []);
+      this.wholesalerTemplates = defaultTemplateWholesaler.map(item =>({...item}));
     } else {
       if (user == 'retailer') {
-        this.deleteListRetailer = this.retailerTemplates;
-        this.retailerTemplates = [];
-        defaultTemplate.map((item: any) => {
-          item.user = 'retailer';
-          return this.retailerTemplates.push(item);
-        });
+        const match = [ ...this.deleteListRetailer, ...this.retailerTemplates]
+        this.deleteListRetailer = match.reduce((a, c) => { !a.find(v => v.id === c.id) && a.push(c); return a; }, []);
+        this.retailerTemplates = defaultTemplateRetailer.map(item =>({...item}));
       }
     }
   }
@@ -206,9 +234,9 @@ export class TemplateMessageComponent {
   async deleteTemplates(body: FormData, user: string) {
     return await this.tms.delete(body).subscribe((res) => {
       console.log('deleted');
-      this.onSaveTemplate(user);
       if(user == "wholesaler") this.deleteListWholesaler = [];
       if(user == "retailer") this.deleteListRetailer = [];
+      this.onSaveTemplate(user);
     }, error => {
       console.log('delete failed', error);
       alert('Failed to Save Template')
@@ -222,6 +250,7 @@ export class TemplateMessageComponent {
     let bodySave = new FormData();
     let bodyDelete = new FormData();
     if (user == "wholesaler") {
+      console.log('this.deleteListWholesaler',this.deleteListWholesaler)
         if (this.deleteListWholesaler.length > 0) {
           let deleteReady = await this.deleteListWholesaler.map((item: any, i: number) => {
             bodyDelete.append('ids['+i+']', item.id);
@@ -254,8 +283,8 @@ export class TemplateMessageComponent {
                   this.isSaved = true;
                   this.onLoad = true;
                   this.tms.get().subscribe((res: any) => {
-                    this.wholesalerTemplates = res.data.wholesaler;
-                    this.retailerTemplates = res.data.retailer;
+                    this.wholesalerTemplates = res.data.wholesaler.length < 5 ? defaultTemplateWholesaler : res.data.wholesaler;
+                    this.retailerTemplates = res.data.retailer < 5 ? defaultTemplateRetailer : res.data.retailer;
                     this.onLoad = false;
                   }, error => {
                     this.onLoad = false;
@@ -314,8 +343,8 @@ export class TemplateMessageComponent {
                 this.isSaved = true;
                 this.onLoad = true;
                 this.tms.get().subscribe((res: any) => {
-                  this.wholesalerTemplates = res.data.wholesaler;
-                  this.retailerTemplates = res.data.retailer;
+                  this.wholesalerTemplates = res.data.wholesaler.length < 5 ? defaultTemplateWholesaler : res.data.wholesaler;
+                  this.retailerTemplates = res.data.retailer < 5 ? defaultTemplateRetailer : res.data.retailer;
                   this.onLoad = false;
                 });
                 setTimeout(() => {
