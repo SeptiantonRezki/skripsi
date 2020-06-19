34ff31cbde518f4a113f58c3ecbc9ab15708a5cc
Yudi <yuddi.0708@gmail.com>
2020-06-19 15:26:22 +0700

    conflict manual verification hq n dev-jatis

--- src/app/pages/dte/template/create/template-create.component.ts
+++ src/app/pages/dte/template/create/template-create.component.ts
@@ -480,6 +480,7 @@ export class TemplateCreateComponent {
         video: this.templateTaskForm.get('video').value? this.templateTaskForm.get('video').value : '',
         is_branching: this.frmIsBranching.value ? 1 : 0,
         is_shareable: this.shareable.value ? 1 : 0,
+        is_ir_template: this.isIRTemplate.value ? 1 : 0,
         questions: questions.map((item, index) => {
           // if (item.question_image) {
           console.log('fioter', this.filteredNext);
