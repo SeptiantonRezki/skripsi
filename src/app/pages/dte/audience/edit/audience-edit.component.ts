3a0f1c7c784d84d0dd8edb9337b8f5c8730d7910
Octa Al Dino R <octa.rangsangpati@jasakelola.com>
2020-10-02 20:38:55 +0700

    Remove Collumn Unuse

--- src/app/pages/dte/audience/edit/audience-edit.component.ts
+++ src/app/pages/dte/audience/edit/audience-edit.component.ts
@@ -41,7 +41,8 @@ export class AudienceEditComponent {
     { name: "NON-SRC", value: "NON-SRC" },
     { name: "IMO", value: "IMO" },
     { name: "LAMP/HOP", value: "LAMP/HOP" },
-    { name: "GT", value: "GT" }
+    { name: "GT", value: "GT" },
+    { name: "KA", value: "KA" }
   ];
   srcClassification: any[] = [
     { name: "Semua Tipe", value: "all" }
@@ -182,6 +183,7 @@ export class AudienceEditComponent {
       limit: [""],
       type: ["", Validators.required],
       audience_type: ["scheduler", Validators.required],
+      geotree_checkbox: true,
       // national: [""],
       // division: [""],
       // region: [""],
