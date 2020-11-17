68a02a33d78bcb8b5a7d009af6ac1247e0c7b46f
oong12370 <oong.julian@gmail.com>
2020-11-17 12:50:32 +0700

    update routing automation tsm

--- src/app/classes/breadcrumbs.config.ts
+++ src/app/classes/breadcrumbs.config.ts
@@ -1448,6 +1448,18 @@ export const brConfig = {
           name: "Buat DTE Automation"
         }
       ],
+      create_tsm: [
+        {
+          type: "parent",
+          url: "/dte/automation",
+          name: "DTE Automation"
+        },
+        {
+          type: "current",
+          url: "",
+          name: "Buat DTE Automation TSM"
+        }
+      ],
       edit: [
         {
           type: "parent",
@@ -1458,6 +1470,18 @@ export const brConfig = {
           type: "current",
           url: "",
           name: "Ubah DTE Automation"
+        }
+      ],
+      edit_tsm: [
+        {
+          type: "parent",
+          url: "/dte/automation",
+          name: "DTE Automation"
+        },
+        {
+          type: "current",
+          url: "",
+          name: "Ubah DTE Automation TSM"
         }
       ],
       detail: [
