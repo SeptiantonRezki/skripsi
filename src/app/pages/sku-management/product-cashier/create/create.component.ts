1383efdea1b0043726eea69a68514ae04c0e8d3d
mavif <muhammad.avif@weekendinc.com>
2021-06-03 13:48:36 +0700

    update new product form

--- src/app/pages/sku-management/product-cashier/create/create.component.ts
+++ src/app/pages/sku-management/product-cashier/create/create.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup } from "@angular/forms";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { MatDialog, MatDialogConfig } from "@angular/material";
 import { DataService } from "app/services/data.service";
 import { ProductCashierService } from "app/services/sku-management/product-cashier.service";
 import { ScanBarcodeDialogComponent } from "app/pages/sku-management/product/create/dialog/scan-barcode-dialog.component";
+import { DialogService } from "app/services/dialog.service";
+import { commonFormValidator } from "app/classes/commonFormValidator";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-cashier-create",
@@ -14,12 +17,15 @@ export class CashierCreateComponent implements OnInit {
   formProductGroup: FormGroup;
   onLoad: boolean = false;
   dialogRef: any;
+  hasBarcode: boolean = false;
 
   constructor(
     private formBuilder: FormBuilder,
     private dataService: DataService,
     private productCashierService: ProductCashierService,
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private dialogService: DialogService,
+    private router: Router
   ) {}
 
   ngOnInit() {
@@ -28,10 +34,10 @@ export class CashierCreateComponent implements OnInit {
 
   createFormGroup() {
     this.formProductGroup = this.formBuilder.group({
-      name: [""],
+      name: ["", Validators.required],
       barcode: [""],
-      purchase_price: [""],
-      selling_price: [""],
+      purchase_price: ["", [Validators.required, Validators.min(1)]],
+      selling_price: ["", [Validators.required, Validators.min(1)]],
     });
   }
 
@@ -44,14 +50,60 @@ export class CashierCreateComponent implements OnInit {
     this.dialogRef = this.dialog.open(ScanBarcodeDialogComponent, dialogConfig);
     this.dialogRef.afterClosed().subscribe((res: any) => {
       if (res) this.getBarcode(res);
-      // this.formProductGroup.get("barcode").setValue(id);
     });
   }
 
   getBarcode(id) {
-    console.log(id);
-    // this.productCashierService.barcode(id).subscribe((res) => {
-    //   console.log(res);
-    // })
+    this.dataService.showLoading(true);
+    this.productCashierService.barcode(id).subscribe(
+      (res) => {
+        if (res.data.length) {
+          this.hasBarcode = true;
+          this.formProductGroup.patchValue({
+            name: res.data[0].name,
+            barcode: res.data[0].barcode,
+          });
+        } else {
+          this.hasBarcode = false;
+          this.dialogService.openSnackBar({
+            message: "Produk tidak ditemukan!",
+          });
+        }
+        this.dataService.showLoading(false);
+      },
+      (err) => {
+        console.log(err);
+        this.dataService.showLoading(false);
+      }
+    );
+  }
+
+  submit() {
+    if (!this.formProductGroup.valid) {
+      this.dialogService.openSnackBar({
+        message: "Silakan lengkapi data terlebih dahulu!",
+      });
+      commonFormValidator.validateAllFields(this.formProductGroup);
+      return;
+    }
+    const body = {
+      name: this.formProductGroup.get('name').value,
+      barcode: this.formProductGroup.get('barcode').value,
+      selling_price: this.formProductGroup.get("selling_price").value,
+      purchase_price: this.formProductGroup.get("purchase_price").value,
+    };
+    this.dataService.showLoading(true);
+    this.productCashierService.create(body).subscribe(
+      (res) => {
+        this.onLoad = false;
+        this.router.navigate(["sku-management", "product-cashier"]);
+        this.dialogService.openSnackBar({ message: "Produk berhasil disimpan" });
+        this.dataService.showLoading(false);
+      },
+      (err) => {
+        this.onLoad = false;
+        this.dataService.showLoading(false);
+      }
+    );
   }
 }
