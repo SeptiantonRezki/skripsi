e9ce73839fc2877725e90c8da5928b5320fb051c
Aswin <aswin.wibisurya@weekendinc.com>
2021-07-30 00:02:35 +0700

    Fix KPI setting create

--- src/app/pages/kpi-setting/kpi-setting.routing.ts
+++ src/app/pages/kpi-setting/kpi-setting.routing.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
     }
   },
   {
+    path: 'kpi-setting-create',
+    component: EditKPISettingComponent
+  },
+  {
     path: 'kpi-setting-edit/:id',
     component: EditKPISettingComponent,
     data: {
