47fa8b0d47bc050f1ac443f17d8523f60d7b4650
Jois Andresky <jois@dxtr.asia>
2021-09-24 17:04:51 +0700

    fixing RCA phase 1, 3 point after deployment

--- src/app/pages/remote-call-activation/grouping-pelanggan/index/grouping-pelanggan-index.component.ts
+++ src/app/pages/remote-call-activation/grouping-pelanggan/index/grouping-pelanggan-index.component.ts
@@ -925,7 +925,7 @@ export class GroupingPelangganIndexComponent implements OnInit {
     if (this.pagination['self_area']) params['self_area'] = this.pagination['self_area'];
     if (this.pagination['last_self_area']) params['last_self_area'] = this.pagination['last_self_area'];
     if (this.pagination['after_level']) params['after_level'] = this.pagination['after_level'];
-    this.rcaAgentService.exportGrouping({ area: this.pagination['area'], ...params, position_code: this.pagination['position'] ? this.pagination['position'] : null }).subscribe(res => {
+    this.rcaAgentService.exportGrouping({ area: this.pagination['area'], ...params, position: this.pagination['position'] ? this.pagination['position'] : null }).subscribe(res => {
       console.log('res', res);
       this.downLoadFile(res, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", `Export_GroupingPelangganRCA_${new Date().toLocaleString()}.xlsx`);
       this.dataService.showLoading(false);
