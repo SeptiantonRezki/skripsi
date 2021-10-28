7bb5c52592c07277423a3c0c5285bc4fec5912ab
ilhamriz <mirizky@pmintl.net>
2021-10-28 18:04:20 +0700

    add menu dynamic pricing

---
+++ src/app/pages/dte/dynamic-pricing/dynamic-pricing.component.ts
@@ -0,0 +1,43 @@
+import { HttpParams } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PagesName } from 'app/classes/pages-name';
+import { AuthenticationService } from 'app/services/authentication.service';
+import { DataService } from 'app/services/data.service';
+import { environment } from 'environments/environment';
+
+@Component({
+  selector: 'app-dynamic-pricing',
+  templateUrl: './dynamic-pricing.component.html',
+  styleUrls: ['./dynamic-pricing.component.scss']
+})
+export class DynamicPricingComponent implements OnInit {
+  url = '';
+  urlSafe: SafeResourceUrl;
+  loading = true;
+  permission: any;
+  roles: PagesName = new PagesName();
+
+  constructor(
+    private sanitizer: DomSanitizer,
+    private authService: AuthenticationService,
+  ) {
+    this.permission = this.roles.getArrayRoles('principal.dtedynamicpricing');
+    this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl('');
+  }
+
+  ngOnInit() {
+    this.authService.getEncryptedToken().subscribe(res => {
+      const baseurl = environment.STREAMLIT;
+
+      const encodedToken = encodeURI(res.data);
+      const httpParams = new HttpParams().set('dceauth', encodedToken).set('platform', 'principal').set('allowBack', '1').set('_prmdxtrn', JSON.stringify(this.permission));
+      const fullUrl = `${baseurl}?${httpParams.toString()}`;
+      // console.log({fullUrl});
+
+      this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(fullUrl);
+      this.loading = false;
+    })
+  }
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -11,7 +11,8 @@ export const environment = {
   qiscus_appId: 'ayosrc-tzvpgcnxyliz1g',
   cognito_login_url: 'https://ayo-principal-prd.auth.ap-southeast-1.amazoncognito.com/oauth2/authorize?identity_provider=ayo-principal-prd&redirect_uri=https://hms.src.id/login&response_type=CODE&client_id=3uvfgpi61hk7a739rtjpjddv70&scope=aws.cognito.signin.user.admin email openid phone profile',
   show_gtm: true,
-  SRC_KATALOG_KOIN_BASE_IFRAME_URL: 'https://hms-react.src.id'
+  SRC_KATALOG_KOIN_BASE_IFRAME_URL: 'https://hms-react.src.id',
+  STREAMLIT: 'http://172.18.104.148:8501',
 };
 
 export const serviceServer = (server) => {
