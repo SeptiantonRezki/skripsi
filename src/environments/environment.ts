24f56d95a2389e467da11eb0a5bdc504ddbb4ea6
Zuhry Abdi Rahmani <zuhryabdirahmani@gmail.com>
2021-12-09 12:03:16 +0700

    change to initial env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  server: "https://api.dev.src.id",
-  server_service: "https://api.dev.src.id",
+  server: 'https://dev.ayo-api.dxtr.asia',
+  server_service: 'https://dev.ayo-api.dxtr.asia',
   label: "DEVELOPMENT",
   show_label: true,
   image: "assets/images/ayo/icon/dev.png",
@@ -11,35 +11,35 @@ export const environment = {
   qiscus_appId: 'ayosrc-8lv9mbp2ce6iwr',
   cognito_login_url: 'https://ayo-principal-dev.auth.ap-southeast-1.amazoncognito.com/oauth2/authorize?identity_provider=ayo-principal-dev&redirect_uri=https://hms.dev.src.id/login&response_type=CODE&client_id=1oe00f63c64c3lpseof1bgcuah&scope=aws.cognito.signin.user.admin email openid phone profile',
   show_gtm: false,
-  SRC_KATALOG_KOIN_BASE_IFRAME_URL: 'https://hms-react.dev.src.id',
+  SRC_KATALOG_KOIN_BASE_IFRAME_URL: 'https://hms-react.ayo-micro.dxtr.asia',
   STREAMLIT: 'https://dynamicpricing.api.dev.src.id',
+  // SRC_KATALOG_KOIN_BASE_IFRAME_URL: 'http://localhost:3000'
 };
 
 export const serviceServer = (server) => {
-  return `https://${server}.api.dev.src.id`;
+  return `https://${server}.ayo-micro.dxtr.asia`;
 }
-// export const serviceServer = (server, paths = ['api'], env='dev') => {
+// export const serviceServer = (server, paths = [], env='ayo-micro') => {
 //   let path = (paths.length) ? `${paths.join('.')}.` : '';
 //   let _env = (env) ? `${env}.` : '';
-//   return `https://${server}.${path}${_env}src.id`;
+//   return `https://${server}.${path}${_env}dxtr.asia`;
 // }
 
 export const server = {
-  user: "users",
-  business: "businesses",
-  area: "areas",
-  auth: "auth",
-  newsfeed: "newsfeed",
-  banner: "banner",
-  content: "contentstatic",
-  product: "product",
-  task: "task-management",
-  submission: "task-submission",
-  coin: "coin-management",
-  community: "community",
-  order: "order",
-  bora: "bora",
+  user: 'user',
+  business: 'business',
+  area: 'area',
+  auth: 'auth',
+  newsfeed: 'newsfeed',
+  banner: 'banner',
+  content: 'content',
+  product: 'product',
+  task: 'task',
+  submission: 'submission',
+  coin: 'coin',
+  community: 'community',
+  order: 'order',
   orderCashier: 'cashier-order',
   productCashier: "cashier-product",
   export: 'export',
-}
+};
