07e0f24665e52ed768c0baf8de6dc6118d44cfba
mmirza2@pmintl.net <mmirza2@pmintl.net>
2022-01-27 18:17:11 +0700

    CDE-4243-country-setup: - download file json

--- src/app/pages/user-management/language-setup/language-setup.component.ts
+++ src/app/pages/user-management/language-setup/language-setup.component.ts
@@ -174,13 +174,36 @@ export class LanguageSetupComponent implements OnInit {
   }
 
   export(row) {
-    this.languageSetupService.export({language_id: row.id}).subscribe(res => {
-      console.log({res});
+    this.dataService.showLoading(true);
+    this.languageSetupService.export({language_id: row.id}).subscribe(({data}) => {
+      
+      console.log({data});
+      this.downLoadFile(data.file);
+      this.dataService.showLoading(false);
+
+    }, err => {
+
+      this.dataService.showLoading(false);
     })
   }
   directDetail(row) {
     this.dataService.setToStorage("language_setup_data", row);
     this.router.navigate(["user-management", "languages", "detail", row.id]);
+  }
+
+  downLoadFile(url) {
+
+    var link = document.createElement('a');
+    link.href = url;
+    // link.download = fileName;
+    // this is necessary as link.click() does not work on the latest firefox
+    link.dispatchEvent(new MouseEvent('click', { bubbles: true, cancelable: true, view: window }));
+
+    setTimeout(function () {
+      // For Firefox it is necessary to delay revoking the ObjectURL
+      window.URL.revokeObjectURL(url);
+      link.remove();
+    }, 100);
   }
 
 }
