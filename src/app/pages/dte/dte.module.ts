ea3727f6228b403d9135e46bcbf9e310bee688d7
Jois Andresky <jjois@pmintl.net>
2022-07-22 16:13:21 +0700

    lottery

--- src/app/pages/dte/dte.module.ts
+++ src/app/pages/dte/dte.module.ts
@@ -173,6 +173,9 @@ import { CoinRedemptionApprovalComponent } from './coin-redemption-approval/coin
 import { EmployeeMappingComponent } from './employee-mapping/employee-mapping.component';
 import { TemplatePlanogramComponent } from './image-recognition/template-planogram/template-planogram.component';
 import { CheckImageComponent } from './image-recognition/check-image/check-image.component';
+import { LotteryComponent } from './lottery/index/lottery.component';
+import { LotteryCreateComponent } from './lottery/create/lottery-create.component';
+import { LotteryEditComponent } from './lottery/edit/lottery-edit.component';
 
 
 export const customCurrencyMaskConfig = {
@@ -362,6 +365,9 @@ export const MY_FORMATS = {
     EmployeeMappingComponent,
     TemplatePlanogramComponent,
     CheckImageComponent,
+    LotteryComponent,
+    LotteryCreateComponent,
+    LotteryEditComponent,
   ],
   exports: [
     TemplateIndexComponent,
---
+++ src/app/pages/dte/lottery/edit/lottery-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lottery-edit',
+  templateUrl: './lottery-edit.component.html',
+  styleUrls: ['./lottery-edit.component.scss']
+})
+export class LotteryEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
