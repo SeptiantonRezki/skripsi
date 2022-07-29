acda3bf397b90ac481a346ba0ca339fb0a3d9162
Fauzi Ferdiansyah <fauzi@dxtr.asia>
2022-07-29 15:43:14 +0700

    [update]:: tab undian add & edit

--- src/app/pages/dte/lottery/create/lottery-create.component.ts
+++ src/app/pages/dte/lottery/create/lottery-create.component.ts
@@ -1,9 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import moment from 'moment';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { TranslateService } from '@ngx-translate/core';
 import { Config } from 'app/classes/config';
 import { LanguagesService } from 'app/services/languages/languages.service';
+import { GroupTradeProgramService } from 'app/services/dte/group-trade-program.service';
+import { LotteryService } from "app/services/dte/lottery.service";
+import { DialogService } from 'app/services/dialog.service';
 import { Observable, ReplaySubject, Subject } from 'rxjs';
+import { commonFormValidator } from 'app/classes/commonFormValidator';
+import { takeUntil } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-lottery-create',
@@ -13,7 +20,9 @@ import { Observable, ReplaySubject, Subject } from 'rxjs';
 export class LotteryCreateComponent implements OnInit {
   selectedTab: number = 0;
 
-  formSpin: FormGroup;
+  formUndian: FormGroup;
+  // formAudience: FormGroup;
+  // formPreview: FormGroup;
   onLoad: boolean;
   minDate = new Date();
   groupTradePrograms: any[] = [];
@@ -28,6 +37,9 @@ export class LotteryCreateComponent implements OnInit {
 
   statusTP: any[] = [{ name: this.translate.instant('dte.trade_program.text6'), value: 'publish' }, { name: this.translate.instant('dte.trade_program.text7'), value: 'unpublish' }]
 
+  listGroupTradeProgram: any[] = [];
+  listSubGroupTradeProgram: any[] = [];
+
   private _onDestroy = new Subject<void>();
   filteredGTpOptions: Observable<string[]>;
   public filterGTP: FormControl = new FormControl();
@@ -39,22 +51,95 @@ export class LotteryCreateComponent implements OnInit {
 
 
   constructor(
+    private router: Router,
     private formBuilder: FormBuilder,
     private ls: LanguagesService,
     private translate: TranslateService,
+    private groupTradeProgramService: GroupTradeProgramService,
+    private lotteryService: LotteryService,
+    private dialogService: DialogService,
   ) {
     this.onLoad = true
   }
 
   ngOnInit() {
-    this.formSpin = this.formBuilder.group({
+    this.formUndian = this.formBuilder.group({
       name: ["", Validators.required],
-      group_trade_id: ["", Validators.required],
+      coin: ["", Validators.required],
+      group_trade_program: [""],
+      sub_group_trade_program: [""],
       start_date: [new Date()],
       end_date: [new Date()],
+      announcement_date:  [new Date()],
     })
+    
+    this.filterGTP.valueChanges
+      .pipe(takeUntil(this._onDestroy))
+      .subscribe(() => {
+        this.filteringGTP();
+      });
+
+    this.filterSGTP.valueChanges
+      .pipe(takeUntil(this._onDestroy))
+      .subscribe(() => {
+        this.filteringSGTP();
+      });
+
+    this.getGroupTradeProgram();
+    this.getSubGroupTradeProgram();
 
     this.onLoad = false;
+  }
+
+  filteringGTP() {
+    if (!this.listGroupTradeProgram) {
+      return;
+    }
+    // get the search keyword
+    let search = this.filterGTP.value;
+    if (!search) {
+      this.filteredGTP.next(this.listGroupTradeProgram.slice());
+      return;
+    } else {
+      search = search.toLowerCase();
+    }
+    // filter the banks
+    this.filteredGTP.next(
+      this.listGroupTradeProgram.filter(item => item.name.toLowerCase().indexOf(search) > -1)
+    );
+  }
+
+  getGroupTradeProgram() {
+    this.groupTradeProgramService.get({ page: 'all' }).subscribe(res => {
+      this.listGroupTradeProgram = res.data ? res.data.data : [];
+      this.filteredGTP.next(this.listGroupTradeProgram.slice());
+    })
+  }
+
+  getSubGroupTradeProgram() {
+    this.groupTradeProgramService.getSubGroupTrade({ page: 'all' }).subscribe(res => {
+      const data = res.data ? res.data.data.filter((item: any) => item.status === "active") : [];
+      this.listSubGroupTradeProgram = data;
+      this.filteredSGTP.next(this.listSubGroupTradeProgram.slice());
+    })
+  }
+  
+  filteringSGTP() {
+    if (!this.listSubGroupTradeProgram) {
+      return;
+    }
+    // get the search keyword
+    let search = this.filterSGTP.value;
+    if (!search) {
+      this.filteredSGTP.next(this.listSubGroupTradeProgram.slice());
+      return;
+    } else {
+      search = search.toLowerCase();
+    }
+    // filter the banks
+    this.filteredSGTP.next(
+      this.listSubGroupTradeProgram.filter(item => item.name.toLowerCase().indexOf(search) > -1)
+    );
   }
 
   removeImage(): void {
@@ -75,6 +160,50 @@ export class LotteryCreateComponent implements OnInit {
     }
 
     myReader.readAsDataURL(file);
+  }
+
+  submit(): void {
+
+    if (this.formUndian.valid) {
+      let fd = new FormData();
+
+      let body = {
+        name: this.formUndian.get('name').value,
+        coin: this.formUndian.get('coin').value,
+        start_date: this.convertDate(this.formUndian.get('start_date').value),
+        end_date: this.convertDate(this.formUndian.get('end_date').value),
+        announcement_date: this.convertDate(this.formUndian.get('announcement_date').value),
+      }
+
+      fd.append('name', body.name);
+      fd.append('coin', body.coin);
+      fd.append('start_date', body.start_date);
+      fd.append('end_date', body.end_date);
+      fd.append('announcement_date', body.announcement_date);
+      fd.append('trade_creator_group_id[]', this.formUndian.get('group_trade_program').value);
+      fd.append('trade_creator_sub_group_id[]', this.formUndian.get('sub_group_trade_program').value);
+
+      this.lotteryService.create(fd).subscribe(
+        res => {
+          this.dialogService.openSnackBar({ message: this.ls.locale.notification.popup_notifikasi.text22 });
+          this.router.navigate(['dte', 'lottery']);
+        },
+        err => {
+          console.log(err.error.message);
+        }
+      )
+    } else {
+      this.dialogService.openSnackBar({ message: this.translate.instant('global.label.please_complete_data') });
+      commonFormValidator.validateAllFields(this.formUndian);
+    }
+  }
+
+  convertDate(param: Date) {
+    if (param) {
+      return moment(param).format('YYYY-MM-DD');
+    }
+
+    return "";
   }
 
 }
