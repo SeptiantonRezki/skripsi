ad015e94fa2d25b36044a59d485dcea56e0bbfa5
Fauzi Ferdiansyah <fauzi@dxtr.asia>
2022-11-17 14:37:00 +0700

    [update]:: logic download task management export

--- src/app/pages/content-management/keyword-management/keyword-management-component.ts
+++ src/app/pages/content-management/keyword-management/keyword-management-component.ts
@@ -319,25 +319,49 @@ export class KeywordManagementComponent implements OnInit {
         category: this.formKeyword.get('category').value,
       };
       const response = await this.keywordService.exportKeyword(body).toPromise();
-      this.downloadLink.nativeElement.href = response.data;
-      this.downloadLink.nativeElement.click();
+      this.downLoadFile(response, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", `Export_Keyword_Management_${new Date().toLocaleString()}.xls`);
       this.dataService.showLoading(false);
     } catch (error) {
+      console.log(error);
       this.handleError(error);
       this.dataService.showLoading(false);
-      // throw error;
     }
   }
 
   handleError(error) {
-    console.log('Here');
-    console.log(error);
-
     if (!(error instanceof HttpErrorResponse)) {
       error = error.rejection;
     }
     console.log(error);
-    // alert('Open console to see the error')
+  }
+
+  downLoadFile(data: any, type: string, fileName: string) {
+    // It is necessary to create a new blob object with mime-type explicitly set
+    // otherwise only Chrome works like it should
+    var newBlob = new Blob([data], { type: type });
+
+    // IE doesn't allow using a blob object directly as link href
+    // instead it is necessary to use msSaveOrOpenBlob
+    if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+      window.navigator.msSaveOrOpenBlob(newBlob);
+      return;
+    }
+
+    // For other browsers:
+    // Create a link pointing to the ObjectURL containing the blob.
+    const url = window.URL.createObjectURL(newBlob);
+
+    var link = document.createElement('a');
+    link.href = url;
+    link.download = fileName;
+    // this is necessary as link.click() does not work on the latest firefox
+    link.dispatchEvent(new MouseEvent('click', { bubbles: true, cancelable: true, view: window }));
+
+    setTimeout(function () {
+      // For Firefox it is necessary to delay revoking the ObjectURL
+      window.URL.revokeObjectURL(url);
+      link.remove();
+    }, 100);
   }
 
   submit() {
