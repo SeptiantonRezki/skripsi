faea20c8c62f9ef13a197b4ad21ba8fd258b8367
Achmad Mawardi <asmaward@pmintl.net>
2023-01-17 15:07:30 +0700

    latest for test create page area configuration in local

--- src/app/pages/control-tower/area-configuration/area-configuration.component.ts
+++ src/app/pages/control-tower/area-configuration/area-configuration.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
-
-
-
+import { Router } from '@angular/router';
+import { DataService } from '../../../services/data.service';
+import { LanguagesService } from 'app/services/languages/languages.service';
+import { TranslateService } from '@ngx-translate/core';
 @Component({
     selector   : 'area-configuration',
     templateUrl: './area-configuration.component.html',
@@ -10,7 +10,13 @@ import { Component } from '@angular/core';
 })
 export class AreaConfiguration
 {
-    constructor()
+    pageName = "Area Configuration";
+    constructor(
+      private router: Router,
+      private dataService: DataService,
+      private ls: LanguagesService,
+      private translate: TranslateService,
+    )
     {
 
     }
